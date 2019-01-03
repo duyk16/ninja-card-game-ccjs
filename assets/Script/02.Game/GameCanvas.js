81ea727cd3af2210eeec499633a13777c219474e
duyk16 <duyk16@gmail.com>
2019-01-03 18:05:08 +0700

    add win screen

--- assets/Script/02.Game/GameCanvas.js
+++ assets/Script/02.Game/GameCanvas.js
@@ -12,20 +12,24 @@ cc.Class({
         main: {
             default: null,
             type: cc.Component
+        },
+        winScreen: {
+            default: null,
+            type: cc.Node
         }
     },
 
     // LIFE-CYCLE CALLBACKS:
     onLoad() {
         this.info = {
-            size: {x: 9, y: 4},
-            diff: 4,
+            size: {x: 8, y: 1},
+            diff: 1,
             point: 10,
             cardTotal() {
                 return this.size.x * this.size.y
             }
         }
-
+        this.winScreen.getComponent('WinScreen').enable = false
     },
     onEnable () {
         const {size, diff} = this.info
@@ -41,19 +45,22 @@ cc.Class({
     gainScore() {
         // Gain score
         this.score += this.info.point
+
         // Show score + animation
         this.scoreDisplay.string = this.score
-        this.scoreDisplay.node.runAction(cc.scaleTo(0.3, 1.2).easing(cc.easeElasticOut(3.0)));
+        this.scoreDisplay.node.runAction(cc.scaleTo(0.2, 1.2).easing(cc.easeElasticOut(3.0)));
         setTimeout(() => {
-            this.scoreDisplay.node.runAction(cc.scaleTo(0.3, 1))
-        }, 300)
+            this.scoreDisplay.node.runAction(cc.scaleTo(0.2, 1))
+        }, 200)
 
         // Count card collect
         this.cardCollect += 2
-
+        // cc.log(this.winScreen.getComponent('WinScreen'))
         // Check win
         if (this.cardCollect >= this.info.cardTotal()) {
-
+            this.winScreen.getComponent('WinScreen').playerScore.string = this.score
+            this.winScreen.active = true
+            this.winScreen.enable = true
         }
     }
     // update (dt) {},
---
+++ assets/Script/02.Game/WinScreen.js
@@ -0,0 +1,32 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        playerScore: {
+            default: null,
+            type: cc.Label
+        },
+        bestScore: {
+            default: null,
+            type: cc.Label
+        },
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad () {
+        this.bestScore.string = '1050'
+        // this.node.scale = 0.9
+        // setTimeout(() => {
+        //     this.node.runAction(cc.scaleTo(0.1, 1))
+        // }, 200)
+    },
+    onEnable() {
+        
+    },
+    start () {
+
+    },
+
+    // update (dt) {},
+});
